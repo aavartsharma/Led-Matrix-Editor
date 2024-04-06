f43f62c655a83881bd153f3851b4bebd3144d380
aavartsharma <rockstarfan0369@gmail.com>
2024-04-07 01:04:14 +0530

    the list showing is done
    the list that give us the hex value list has been done but have some bug

--- script.js
+++ script.js
@@ -1,146 +1,160 @@
+let moduleNumber = 4; 
+let colorOfButton; 
+let colorNumber = 10;
+let BinaryList = [];
+//console.log(parseInt(11, 2));
 
-
-for (let i = 1; i <= 8; i++) 
+for (let i = 0; i < moduleNumber; i++) 
 {
-    let div1 = document.createElement("div");
-    div1.classList.add("checkkkk");
-    for (let j = 1; j <= 8; j++) 
+  // console.log(BinaryList);
+  let moduleDiv = document.createElement("div");
+  moduleDiv.classList.add("displayclass");
+  moduleDiv.id = "display"+String(i+1);
+  let container = document.getElementById("displaygrid");
+  container.appendChild(moduleDiv);
+  let arrayList = [];
+  for(let k =0;k < 8;k++)
+  {
+    let div = document.createElement("div");
+    div.classList.add("checkkkk");
+    let innerlist = [];
+    for (let j = 0; j < 8; j++) 
     {
-        // Create a span element
-        let span = document.createElement("span");
-
-        // Create a button element
-        let button = document.createElement("button");
-
-        // Add a class to the button
-        button.classList.add("pixel");
-
-        // Add event listener to the button for the "click" event
-        button.addEventListener("click", () => {
-          // Change the background color of the button
-          if(window.getComputedStyle(button).backgroundColor == 'rgb(0, 0, 255)')
+      // Create a span element
+      let span = document.createElement("span");
+      
+      // Create a button element
+      let button = document.createElement("button");
+      
+      // Add a class to the button
+      button.classList.add("pixel");
+      // Add event listener to the button for the "click" event
+      innerlist[j] = "0";
+      button.addEventListener("click", () => {
+        // Change the background color of the button
+        if (window.getComputedStyle(button).backgroundColor != 'rgb(255, 255, 255)') 
+        {
+          button.style.backgroundColor = "#ffffff"; // change to white
+          BinaryList[i][k][j] = "0";
+          
+        }
+        else if (window.getComputedStyle(button).backgroundColor == 'rgb(255, 255, 255)')
+        {
+          if(colorOfButton == null)
           {
-            button.style.backgroundColor = "#ffffff"; // change to white
+            button.style.backgroundColor = "#0000ff";
           }
-          else if(window.getComputedStyle(button).backgroundColor == 'rgb(255, 255, 255)')
+          else
           {
-            button.style.backgroundColor = "#0000ff"; // Change color to red
+            button.style.backgroundColor = colorOfButton; // Change color to the random Color
           }
-          console.log(window.getComputedStyle(button).backgroundColor); // get the backgroundcolor
-        });
-        // Append the button to the span
-        span.appendChild(button);
-        div1.appendChild(span);
+          BinaryList[i][k][j] = "1";
+          let fffff= makeHexString(BinaryList[i]);
+          let divDisplayer = document.createElement("div");
+          let prentDiv = document.getElementById("DisplayContainer");
+          divDisplayer.textContent = fffff;
+          prentDiv.appendChild(divDisplayer);
+        }
+      });
+      span.appendChild(button);
+      div.appendChild(span);
     }
-
-    let container1 = document.getElementById("display1");
-    container1.appendChild(div1);
-
-    let div2 = document.createElement("div");
-    div2.classList.add("checkkkk");
-
-    for (let j = 1; j <= 8; j++) 
-    {
-        // Create a span element
-        let span = document.createElement("span");
-
-        // Create a button element
-        let button = document.createElement("button");
-
-        // Add a class to the button
-        button.classList.add("pixel");
-        button.id = "pixel-" + String(j);
-
-        // Add event listener to the button for the "click" event
-        button.addEventListener("click", () => {
-          // Change the background color of the button
-          if(window.getComputedStyle(button).backgroundColor == 'rgb(0, 0, 255)')
-          {
-            button.style.backgroundColor = "#ffffff";
-          }
-          else if(window.getComputedStyle(button).backgroundColor == 'rgb(255, 255, 255)')
-          {
-            button.style.backgroundColor = "#0000ff"; // Change color to red
-          }
-          console.log(window.getComputedStyle(button).backgroundColor);
-        });
-        // Append the button to the span
-        span.appendChild(button);
-        div2.appendChild(span);
-    }
-
-    let container2 = document.getElementById("display2");
-    container2.appendChild(div2);
-
-    let div3 = document.createElement("div");
-    div3.classList.add("checkkkk");
-
-    for (let j = 1; j <= 8; j++) 
-    {
-        // Create a span element
-        let span = document.createElement("span");
-
-        // Create a button element
-        let button = document.createElement("button");
-
-        // Add a class to the button
-        button.classList.add("pixel");
-        button.id = "pixel-" + String(j);
-
-        // Add event listener to the button for the "click" event
-        button.addEventListener("click", () => {
-          // Change the background color of the button
-          if(window.getComputedStyle(button).backgroundColor == 'rgb(0, 0, 255)')
-          {
-            button.style.backgroundColor = "#ffffff";
-          }
-          else if(window.getComputedStyle(button).backgroundColor == 'rgb(255, 255, 255)')
-          {
-            button.style.backgroundColor = "#0000ff"; // Change color to red
-          }
-          console.log(window.getComputedStyle(button).backgroundColor);
-        });
-        // Append the button to the span
-        span.appendChild(button);
-        div3.appendChild(span);
-    }
-
-    let container3 = document.getElementById("display3");
-    container3.appendChild(div3);
-
-    let div4 = document.createElement("div");
-    div4.classList.add("checkkkk");
-
-    for (let j = 1; j <= 8; j++) 
-    {
-        // Create a span element
-        let span = document.createElement("span");
-
-        // Create a button element
-        let button = document.createElement("button");
-
-        // Add a class to the button
-        button.classList.add("pixel");
-        button.id = "pixel-" + String(j);
-
-        // Add event listener to the button for the "click" event
-        button.addEventListener("click", () => {
-          // Change the background color of the button
-          if(window.getComputedStyle(button).backgroundColor == 'rgb(0, 0, 255)')
-          {
-            button.style.backgroundColor = "#ffffff";
-          }
-          else if(window.getComputedStyle(button).backgroundColor == 'rgb(255, 255, 255)')
-          {
-            button.style.backgroundColor = "#0000ff"; // Change color to red
-          }
-          console.log(window.getComputedStyle(button).backgroundColor);
-        });
-        // Append the button to the span
-        span.appendChild(button);
-        div4.appendChild(span);
-    }
-
-    let container4 = document.getElementById("display4");
-    container4.appendChild(div4);
+    arrayList.push(innerlist);
+    moduleDiv.appendChild(div);
+  }
+  BinaryList.push(arrayList);
+  
 }
+
+for(let i = 1; i <= moduleNumber*4;i++) // for side counting bar
+{
+  let div = document.createElement("div");
+  div.classList.add("numberclass");
+  div.textContent = i;
+  let container = document.getElementById("linecount");
+  container.appendChild(div);
+
+}
+
+for (let i = 1; i <= colorNumber;i++)
+{
+  let button = document.createElement("button"); //colorButtons
+  button.textContent = "Change";
+  let randomColor = getRandomColor();
+  button.style.backgroundColor = randomColor;
+  button.addEventListener("click", () => {
+    colorOfButton = randomColor;
+  });
+  let div = document.createElement("div");
+  div.classList.add("colorButton");
+  div.appendChild(button);
+  let container = document.getElementById("colorButtons");
+  container.appendChild(div);
+
+}
+
+function makeHexString(List)
+{
+  let Astring = "";
+  List.forEach(bit => {
+    let ff = BinaryListToHex(bit);
+    let extra = "0x";
+    if(ff < 10)
+    {
+      extra = "0x0";
+    }
+    Astring += extra + ff+",";
+  });
+  return Astring;
+}
+
+
+
+function BinaryListToHex(List)
+{
+  let string = "";
+  List.forEach(bit=> {
+    string += bit;
+  });
+  let hexaNumber = binaryToHex(string);
+  return hexaNumber;
+
+}
+
+function binaryToHex(binary) {
+  // Convert binary to decimal
+  let decimal = parseInt(binary, 2);
+  
+  // Convert decimal to hexadecimal
+  let hexadecimal = decimal.toString(16);
+  return hexadecimal;
+}
+
+function getRandomColor() 
+{
+  // Generate random values for red, green, and blue components
+  let red = Math.floor(Math.random() * 256); // Random value between 0 and 255
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+
+  // Convert values to hexadecimal strings
+  let redHex = red.toString(16).padStart(2, '0'); // Convert to hexadecimal and pad with leading zero if necessary
+  let greenHex = green.toString(16).padStart(2, '0');
+  let blueHex = blue.toString(16).padStart(2, '0');
+
+  //Concatenate components and return the color string
+  let color = '#' + redHex + greenHex + blueHex;
+  return color;
+}
+
+// Generate a random integer between min (inclusive) and max (inclusive)
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+
+
+
